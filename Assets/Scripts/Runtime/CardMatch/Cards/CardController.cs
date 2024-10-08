e9a4ba9c8cbfe67bb46d8e81a309ae08f2890972
henritar <henriquetorres92@gmail.com>
2024-10-08 18:45:09 -0300

    Added observable to catch mouse click on card

--- Assets/Scripts/Runtime/CardMatch/Cards/CardController.cs
+++ Assets/Scripts/Runtime/CardMatch/Cards/CardController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using Zenject;
+using UniRx;
 
 namespace Assets.Scripts.Runtime.CardMatch.Cards
 {
@@ -8,21 +9,30 @@ namespace Assets.Scripts.Runtime.CardMatch.Cards
 
         private readonly CardModel _cardModel;
         private readonly CardView _cardView;
+        private readonly Camera _camera;
 
-        public CardController(CardModel cardModel, CardView cardView)
+        private bool _isFlipped;
+
+        public CardController(CardModel cardModel, CardView cardView, Camera camera)
         {
             _cardModel = cardModel;
             _cardView = cardView;
+            _camera = camera;
         }
 
         public void Initialize()
         {
-          
+            Observable.EveryUpdate()
+           .Where(_ => Input.GetMouseButtonDown(0))
+           .Select(_ => _camera.ScreenToWorldPoint(Input.mousePosition))
+           .Subscribe(OnClick)
+           .AddTo(_cardView);
         }
 
         public void Flip()
         {
-            throw new System.NotImplementedException();
+            Debug.Log("Clicked card name: " + _cardView.name);
+            _isFlipped = !_isFlipped;
         }
 
         public bool Compare(Sprite otherCardSprite)
@@ -33,6 +43,16 @@ namespace Assets.Scripts.Runtime.CardMatch.Cards
         public void Animate()
         {
             throw new System.NotImplementedException();
+        }
+
+        private void OnClick(Vector3 clickPosition)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(clickPosition, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject == _cardView.gameObject)
+            {
+                Flip();
+            }
         }
     }
 }
